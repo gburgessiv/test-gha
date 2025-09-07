5c09b1abefb60efa01ef65fa3f85f7d80ab5f801
George Burgess IV <george.burgess.iv@gmail.com>
2025-09-06 18:25:43 -0600

    automatically verify the validity of yaml files

---
+++ email-rotation/verify_yaml_files_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import unittest
+
+import rotations
+
+class TestRotationYamlFilesParse(unittest.TestCase):
+    """Ensures our yaml files parse."""
+
+    def test_rotation_members_yaml_parses(self):
+        parsed = rotations.RotationMembersFile.parse_file(rotations.ROTATION_MEMBERS_FILE)
+        self.assertTrue(parsed.members, "No rotation members could be parsed")
+
+    def test_rotation_yaml_parses(self):
+        parsed = rotations.RotationFile.parse_file(rotations.ROTATION_FILE)
+        self.assertTrue(parsed.rotations, "No rotations could be parsed")
+
+if __name__ == "__main__":
+    unittest.main()
