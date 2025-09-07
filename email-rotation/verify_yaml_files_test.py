87d7dbc25454abc9c21db6f18aa83b77a1cd0f66
George Burgess IV <george.burgess.iv@gmail.com>
2025-09-06 18:43:04 -0600

    appease formatter

--- email-rotation/verify_yaml_files_test.py
+++ email-rotation/verify_yaml_files_test.py
@@ -9,12 +9,15 @@ class TestRotationYamlFilesParse(unittest.TestCase):
     """Ensures our yaml files parse."""
 
     def test_rotation_members_yaml_parses(self) -> None:
-        parsed = rotations.RotationMembersFile.parse_file(rotations.ROTATION_MEMBERS_FILE)
+        parsed = rotations.RotationMembersFile.parse_file(
+            rotations.ROTATION_MEMBERS_FILE
+        )
         self.assertTrue(parsed.members, "No rotation members could be parsed")
 
     def test_rotation_yaml_parses(self) -> None:
         parsed = rotations.RotationFile.parse_file(rotations.ROTATION_FILE)
         self.assertTrue(parsed.rotations, "No rotations could be parsed")
+
 
 if __name__ == "__main__":
     unittest.main()
